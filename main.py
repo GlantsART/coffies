35ea1aabe6009ab65162efe163234ab66b3fc9f0
Гланц Артём <glants2008@yandex.ru>
2024-12-01 14:41:37 +0700

    Скелет программы

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print(13)
+import sys
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class CoffiesInfo(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.pushButton.clicked.connect(self.shown)
+
+    def shown(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = CoffiesInfo()
+    ex.show()
+    sys.exit(app.exec())
